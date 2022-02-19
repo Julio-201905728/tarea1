3d1126a979c043c154f8bbc9254c7f9b1f89b2dd
Julio-201905728 <100011318+Julio-201905728@users.noreply.github.com>
2022-02-18 22:56:35 -0600

    Calculo de un Factorial

---
+++ clase 09.0.2.2022/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, j, n As Integer
+        Dim Esperar As Char
+        While Esperar <> "z" And Esperar <> "Z" And Esperar <> "H"
+            Try
+                j = 1
+                Console.Clear()
+                'formula para calculo n! = 1 x 2 x 3 x 4 x 5 x ... x (n-1) x n
+                Console.Write("Ingresar un numero entero positivo para calcular el factorial ")
+                n = Console.ReadLine()
+                For i = 1 To n Step 1
+                    j = j * i
+                Next
+
+                Console.Write("El factorial del numero ingresado es= ")
+                Console.WriteLine(j)
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+            End Try
+            Console.Write("Presione la letra z , Z o H para salir del programa")
+            Esperar = Console.ReadKey.KeyChar
+        End While
+    End Sub
+
+End Module
